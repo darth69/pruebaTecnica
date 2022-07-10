f65163c88cf853167b0260ddb3c8cd8b2ec8880d
darth69 <torregrosa.f@gmail.com>
2022-07-10 22:26:46 +0200

    Feat: Implementar Lombok
    Implementar Lombok y añadir log de arranque y parada

--- src/main/java/com/hotelbeds/pruebaTecnica/PruebaTecnicaApplication.java
+++ src/main/java/com/hotelbeds/pruebaTecnica/PruebaTecnicaApplication.java
@@ -1,13 +1,24 @@
 package com.hotelbeds.pruebaTecnica;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class PruebaTecnicaApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaTecnicaApplication.class, args);
+		
+		log.info(">>>> Iniciando Prueba Tecnica");
+
+		
+		log.info("<<<< Finalizando Prueba Tecnica");
+		
 	}
 
 }
