7b0f4b4a5951323877ba93d44ebbe3301a4987f0
darth69 <torregrosa.f@gmail.com>
2022-07-18 00:56:48 +0200

    Feat: Implementar DetectorFromFile
    Implementar DetectorFromFile

--- src/main/java/com/hotelbeds/supplierintegrations/hackertest/application/detector/DetectorFromFile.java
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/application/detector/DetectorFromFile.java
@@ -1,19 +1,69 @@
 package com.hotelbeds.supplierintegrations.hackertest.application.detector;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 
+import com.hotelbeds.supplierintegrations.hackertest.application.utils.UtilsDateTime;
 import com.hotelbeds.supplierintegrations.hackertest.model.Ip;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class DetectorFromFile implements DetectorFactory {
 
-	@Value("")
-	public String rutaAlmacenIps;
-	
+	@Value("${detector.ip.storage.path}")
+	private String rutaAlmacenIps;
+		
 	@Override
 	public boolean analizeIp(Ip ip, LocalDateTime eventDateTime) {
 		
+		File dir = new File(rutaAlmacenIps);
+		
+		if(!dir.exists()) {
+			log.info("Creando directorio inexistente -> " + rutaAlmacenIps);
+			dir.mkdirs();
+			log.info("Directorio " + rutaAlmacenIps + " creado");
+		}
+		
+		File file = new File(rutaAlmacenIps + "/" + ip.getIp() + ".txt");		
+				
+		List<LocalDateTime> events = recoveryEventsForIp(file).stream().map(UtilsDateTime::parseLocalDateTimeEvent).collect(Collectors.toList());
+		
 		return false;
+	}
+
+	private ArrayList<String> recoveryEventsForIp(File file) {
+		ArrayList<String> events = new ArrayList<>();
+		if(file.exists()) {
+			log.info("Cargando Ips a memoria");
+			BufferedReader bufferedReader;			
+			try {
+				bufferedReader = new BufferedReader(new FileReader(file));
+				String line;
+				while((line = bufferedReader.readLine()) != null) { 
+					events.add(line);
+					log.debug("Epoch Cargado -> " + line);
+					}
+				bufferedReader.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();				
+			}
+			log.info("Ips cargadas en memoria");			
+		}
+		return events;
 	}
 }
--- src/test/java/com/hotelbeds/supplierintegrations/hackertest/detector/DetectorFromFileTest.java
+++ src/test/java/com/hotelbeds/supplierintegrations/hackertest/detector/DetectorFromFileTest.java
@@ -1,25 +1,38 @@
 package com.hotelbeds.supplierintegrations.hackertest.detector;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.hotelbeds.supplierintegrations.hackertest.application.detector.DetectorFromFile;
+
 import com.hotelbeds.supplierintegrations.hackertest.model.Ip;
 
 @ExtendWith(MockitoExtension.class)
 public class DetectorFromFileTest {
 	
+	private static final String IP_192_168_1_0 = "192.168.1.0";
+
 	@InjectMocks
 	DetectorFromFile detectorFromFile;
 
+	@BeforeEach
+	private void initTest() {
+		ReflectionTestUtils.setField(detectorFromFile, "rutaAlmacenIps", "/ips");
+	}
+	
 	@Test
 	public void returnFalse() {
-		assertThat(detectorFromFile.analizeIp(new Ip("192.168.1.0"), LocalDateTime.now())).as("returnFalse").isFalse();
+		
+		assertThat(detectorFromFile.analizeIp(new Ip(IP_192_168_1_0), LocalDateTime.now())).as("returnFalse").isFalse();
 	}
 }
