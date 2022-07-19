aed1a7c5c777a9eeea68d49100ab081b15bd5227
darth69 <torregrosa.f@gmail.com>
2022-07-19 22:08:17 +0200

    Feat: Refact metodo recuperar eventos
    Se refactoriza para implementar el nuevo servicio de recuperar eventos.

--- src/main/java/com/hotelbeds/supplierintegrations/hackertest/application/detector/DetectorFromFile.java
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/application/detector/DetectorFromFile.java
@@ -1,20 +1,17 @@
 package com.hotelbeds.supplierintegrations.hackertest.application.detector;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
-import com.hotelbeds.supplierintegrations.hackertest.application.utils.UtilsDateTime;
+import com.hotelbeds.supplierintegrations.hackertest.application.fileprocesors.FileEventReader;
+import com.hotelbeds.supplierintegrations.hackertest.application.utils.datetime.UtilsDateTime;
+import com.hotelbeds.supplierintegrations.hackertest.application.utils.file.UtilsFile;
 import com.hotelbeds.supplierintegrations.hackertest.model.Ip;
 
 import lombok.extern.slf4j.Slf4j;
@@ -22,48 +19,38 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DetectorFromFile implements DetectorFactory {
 
+	private static final long TRESCIENTOS_PLUS = 300L;
+
+	private static final long TRECIENTOS_MINUS = -300L;
+
 	@Value("${detector.ip.storage.path}")
 	private String rutaAlmacenIps;
+	
+	@Autowired
+	private FileEventReader fileEventReader;
+	
+	@Autowired
+	private UtilsDateTime utilsDateTime;
+	
+	@Autowired
+	private UtilsFile utilsFile;
 		
 	@Override
 	public boolean analizeIp(Ip ip, LocalDateTime eventDateTime) {
 		
-		File dir = new File(rutaAlmacenIps);
+		//Crear Objeto para tratamiento del almacen
+		utilsFile.CrearDirectorios(rutaAlmacenIps);
 		
-		if(!dir.exists()) {
-			log.info("Creando directorio inexistente -> " + rutaAlmacenIps);
-			dir.mkdirs();
-			log.info("Directorio " + rutaAlmacenIps + " creado");
-		}
-		
+		// Crear Objeto para cargar los eventos
 		File file = new File(rutaAlmacenIps + "/" + ip.getIp() + ".txt");		
 				
-		List<LocalDateTime> events = recoveryEventsForIp(file).stream().map(UtilsDateTime::parseLocalDateTimeEvent).collect(Collectors.toList());
+		//Cargar eventos
+		List<LocalDateTime> events = fileEventReader.recoveryEventsForIp(file).stream().map(utilsDateTime::parseLocalDateTimeEvent).collect(Collectors.toList());
 		
-		return false;
-	}
-
-	private ArrayList<String> recoveryEventsForIp(File file) {
-		ArrayList<String> events = new ArrayList<>();
-		if(file.exists()) {
-			log.info("Cargando Ips a memoria");
-			BufferedReader bufferedReader;			
-			try {
-				bufferedReader = new BufferedReader(new FileReader(file));
-				String line;
-				while((line = bufferedReader.readLine()) != null) { 
-					events.add(line);
-					log.debug("Epoch Cargado -> " + line);
-					}
-				bufferedReader.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();				
-			}
-			log.info("Ips cargadas en memoria");			
-		}
-		return events;
-	}
+		Long res = events.stream().map(event -> ChronoUnit.SECONDS.between(event, eventDateTime))
+				.filter(event -> event >= TRECIENTOS_MINUS && event <= TRESCIENTOS_PLUS)				
+				.count();
+		
+		return res > 0;		
+	}	
 }
--- src/test/java/com/hotelbeds/supplierintegrations/hackertest/detector/DetectorFromFileTest.java
+++ src/test/java/com/hotelbeds/supplierintegrations/hackertest/detector/DetectorFromFileTest.java
@@ -1,38 +1,132 @@
 package com.hotelbeds.supplierintegrations.hackertest.detector;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import com.hotelbeds.supplierintegrations.hackertest.application.detector.DetectorFromFile;
-
+import com.hotelbeds.supplierintegrations.hackertest.application.fileprocesors.FileEventReader;
+import com.hotelbeds.supplierintegrations.hackertest.application.utils.datetime.UtilsDateTime;
+import com.hotelbeds.supplierintegrations.hackertest.application.utils.file.UtilsFile;
 import com.hotelbeds.supplierintegrations.hackertest.model.Ip;
 
 @ExtendWith(MockitoExtension.class)
 public class DetectorFromFileTest {
 	
+	private static final long LONG_300PLUS = 300L; 
+	private static final long LONG_300MINUS = -300L;
+	private static final long LONG_301PLUS = 301L; 
+	private static final long LONG_301MINUS = -301L; 
+
 	private static final String IP_192_168_1_0 = "192.168.1.0";
 
+	@Value("${detector.ip.storage.path}")
+	private String rutaAlmacenIps;
+	
 	@InjectMocks
 	DetectorFromFile detectorFromFile;
+	
+	@Mock
+	FileEventReader fileEventReader;
 
+	@Mock
+	UtilsDateTime utilsDateTime;
+	
+	@Mock
+	UtilsFile utilsFile;
+	
 	@BeforeEach
 	private void initTest() {
 		ReflectionTestUtils.setField(detectorFromFile, "rutaAlmacenIps", "/ips");
 	}
 	
 	@Test
-	public void returnFalse() {
+	public void shouldTrue300Plus() {
 		
-		assertThat(detectorFromFile.analizeIp(new Ip(IP_192_168_1_0), LocalDateTime.now())).as("returnFalse").isFalse();
+		ArrayList<String> events = new ArrayList<>();
+		
+		LocalDateTime ahora = LocalDateTime.now();
+		
+		ZonedDateTime ahoraZoned5 = ahora.plusSeconds(LONG_300PLUS).atZone(ZoneId.systemDefault());
+		Long now5 = ahoraZoned5.toInstant().toEpochMilli();
+		events.add(now5.toString());
+		
+		
+		when(fileEventReader.recoveryEventsForIp(any())).thenReturn(events);
+		when(utilsDateTime.parseLocalDateTimeEvent(any())).thenReturn(ahora.plusSeconds(LONG_300PLUS));
+		doNothing().when(utilsFile).CrearDirectorios(any());
+		
+		assertThat(detectorFromFile.analizeIp(new Ip(IP_192_168_1_0), ahora)).as("shouldTrue300Plus").isTrue();
+	}
+	
+	@Test
+	public void shouldTrue300Minus() {
+		
+		ArrayList<String> events = new ArrayList<>();
+		
+		LocalDateTime ahora = LocalDateTime.now();
+		
+		ZonedDateTime ahoraZoned5 = ahora.plusSeconds(LONG_300MINUS).atZone(ZoneId.systemDefault());
+		Long now5 = ahoraZoned5.toInstant().toEpochMilli();
+		events.add(now5.toString());
+		
+		
+		when(fileEventReader.recoveryEventsForIp(any())).thenReturn(events);
+		when(utilsDateTime.parseLocalDateTimeEvent(any())).thenReturn(ahora.plusSeconds(LONG_300MINUS));
+		
+		assertThat(detectorFromFile.analizeIp(new Ip(IP_192_168_1_0), ahora)).as("shouldTrue300Minus").isTrue();
+	}
+	
+	@Test
+	public void shouldFalse301Minus() {
+		
+		ArrayList<String> events = new ArrayList<>();
+		
+		LocalDateTime ahora = LocalDateTime.now();
+				
+		ZonedDateTime ahoraZoned5 = ahora.plusSeconds(LONG_301MINUS).atZone(ZoneId.systemDefault());
+		Long now5 = ahoraZoned5.toInstant().toEpochMilli();
+		events.add(now5.toString());
+		
+		
+		when(fileEventReader.recoveryEventsForIp(any())).thenReturn(events);
+		when(utilsDateTime.parseLocalDateTimeEvent(any())).thenReturn(ahora.plusSeconds(LONG_301MINUS));
+		
+		assertThat(detectorFromFile.analizeIp(new Ip(IP_192_168_1_0), ahora)).as("shouldFalse301Minus").isFalse();
+	}
+
+	@Test
+	public void shouldFalse301Plus() {
+		
+		ArrayList<String> events = new ArrayList<>();
+		
+		LocalDateTime ahora = LocalDateTime.now();
+		
+		ZonedDateTime ahoraZoned5 = ahora.plusSeconds(LONG_301PLUS).atZone(ZoneId.systemDefault());
+		Long now5 = ahoraZoned5.toInstant().toEpochMilli();
+		events.add(now5.toString());
+		
+		
+		when(fileEventReader.recoveryEventsForIp(any())).thenReturn(events);
+		when(utilsDateTime.parseLocalDateTimeEvent(any())).thenReturn(ahora.plusSeconds(LONG_301PLUS));
+		
+		assertThat(detectorFromFile.analizeIp(new Ip(IP_192_168_1_0), ahora)).as("shouldFalse301Plus").isFalse();
 	}
 }
