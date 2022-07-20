9025ee3e3f3ad6631c78edce893d94e545bfc125
darth69 <torregrosa.f@gmail.com>
2022-07-20 18:59:40 +0200

    Feat: Test testMain

---
+++ src/test/java/com/hotelbeds/pruebaTecnica/PruebaTecnicaApplicationTest.java
@@ -0,0 +1,13 @@
+package com.hotelbeds.pruebaTecnica;
+
+import org.junit.jupiter.api.Test;
+
+public class PruebaTecnicaApplicationTest {
+
+	@Test
+	public void testMain() {
+		//TODO: Implement full test
+		
+		PruebaTecnicaApplication.main(new String[] {});		
+	}
+}
