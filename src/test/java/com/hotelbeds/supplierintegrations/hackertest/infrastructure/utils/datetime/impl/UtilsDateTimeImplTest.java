af4a6a9ec779a0b7dcf8eaa818e066a048939454
darth69 <torregrosa.f@gmail.com>
2022-07-20 19:01:09 +0200

    Feat: Ajuste de Test

--- src/test/java/com/hotelbeds/supplierintegrations/hackertest/infrastructure/utils/datetime/impl/UtilsDateTimeImplTest.java
+++ src/test/java/com/hotelbeds/supplierintegrations/hackertest/infrastructure/utils/datetime/impl/UtilsDateTimeImplTest.java
@@ -6,6 +6,8 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -29,6 +31,26 @@ public class UtilsDateTimeImplTest {
 		LocalDateTime expected = Instant.ofEpochMilli(nowEpoch).atZone(ZoneId.systemDefault()).toLocalDateTime();
 		
 		assertThat(utilsDateTimeImpl.parseLocalDateTimeEvent(nowEpoch.toString())).isEqualTo(expected);
+		
+	}
+	
+	@Test
+	public void sholudOrderLocalDateTimeList() {
+		
+		LocalDateTime min = LocalDateTime.now();
+		LocalDateTime max = min.plusDays(1L);
+		
+		List<LocalDateTime> unsorted = new ArrayList<>();
+		unsorted.add(min);
+		unsorted.add(max);
+
+		List<LocalDateTime> sorted = new ArrayList<>();
+		sorted.add(max);
+		sorted.add(min);
+
+		assertThat(utilsDateTimeImpl.orderLocalDateTimeList(unsorted))
+				.as("sholudOrderLocalDateTimeList")
+				.isEqualTo(sorted);
 		
 	}
 
