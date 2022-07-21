a270a3af69aaf6fc040c85700452314d9634d214
darth69 <torregrosa.f@gmail.com>
2022-07-21 02:15:40 +0200

    Feat: Crear Test
    Pendiente de Implementar

---
+++ src/test/java/com/hotelbeds/supplierintegrations/hackertest/infrastructure/fileeventengine/impl/FileEventWriterImplTest.java
@@ -0,0 +1,12 @@
+package com.hotelbeds.supplierintegrations.hackertest.infrastructure.fileeventengine.impl;
+
+import org.junit.jupiter.api.Test;
+
+public class FileEventWriterImplTest {
+
+	@Test
+	public void testWriteEvents() {
+		//TODO: Implement Test
+	}
+
+}
