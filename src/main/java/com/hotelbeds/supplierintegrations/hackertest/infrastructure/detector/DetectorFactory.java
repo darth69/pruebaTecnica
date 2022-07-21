0366ef0a50f9e968239b9a7b6dcc4cdf61979eb7
darth69 <torregrosa.f@gmail.com>
2022-07-21 21:17:09 +0200

    Feat: quitar imports sin uso

--- src/main/java/com/hotelbeds/supplierintegrations/hackertest/infrastructure/detector/DetectorFactory.java
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/infrastructure/detector/DetectorFactory.java
@@ -1,6 +1,5 @@
 package com.hotelbeds.supplierintegrations.hackertest.infrastructure.detector;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 
 import com.hotelbeds.supplierintegrations.hackertest.model.Ip;
--- src/main/java/com/hotelbeds/supplierintegrations/hackertest/infrastructure/fileeventengine/FileEventReader.java
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/infrastructure/fileeventengine/FileEventReader.java
@@ -1,8 +1,6 @@
 package com.hotelbeds.supplierintegrations.hackertest.infrastructure.fileeventengine;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 
 public interface FileEventReader {
