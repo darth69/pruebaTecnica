ad52a5cf0dcb6e3f2ce1cc05ebb5f2fa1eef3af7
darth69 <torregrosa.f@gmail.com>
2022-07-24 01:42:35 +0200

    Feat: Creacion de DetectorEngine

--- src/main/java/com/hotelbeds/supplierintegrations/hackertest/application/detector/DetectorEngine.java
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/application/detector/DetectorEngine.java
@@ -1,26 +1,10 @@
 package com.hotelbeds.supplierintegrations.hackertest.application.detector;
 
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
+public interface DetectorEngine {
 
-public class DetectorEngine {
-	
-	@Value("${detector.ip.max.limit}")
-	private long maxLimit;
+	boolean detectIp(List<LocalDateTime> events, LocalDateTime eventDateTime);
 
-	@Value("${detector.ip.min.limit}")
-	private long minLimit;
-
-	@Value("${detector.ip.retry.limit}")
-	private long retryLimit;
-	
-	public boolean detectIp(List<LocalDateTime> events, LocalDateTime eventDateTime) {
-		Long res = events.stream().map(event -> ChronoUnit.SECONDS.between(event, eventDateTime))
-				.filter(event -> event >= minLimit && event <= maxLimit)				
-				.count();
-		return res >= retryLimit;
-	}
 }
---
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/application/detector/DetectorEngineImpl.java
@@ -0,0 +1,29 @@
+package com.hotelbeds.supplierintegrations.hackertest.application.detector;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.hotelbeds.supplierintegrations.hackertest.infrastructure.configurator.ConfigLoader;
+
+@Service
+public class DetectorEngineImpl implements DetectorEngine{
+	
+	
+	
+	@Autowired
+	private ConfigLoader configLoader;
+	
+	@Override
+	public boolean detectIp(List<LocalDateTime> events, LocalDateTime eventDateTime) {
+		Long res = events.stream().map(event -> ChronoUnit.SECONDS.between(event, eventDateTime))
+				.filter(event -> event >= configLoader.getMinLimit() && event <= configLoader.getMaxLimit())				
+				.count();
+		
+		return res >= configLoader.getRetryLimit(); 
+	}
+}
