cd4421725a09419956dea10ccc9f1b3ff6257a80
darth69 <torregrosa.f@gmail.com>
2022-07-24 01:32:02 +0200

    Feat: Ajustar Test

---
+++ src/test/java/com/hotelbeds/supplierintegrations/hackertest/PruebaTecnicaApplicationTest.java
@@ -0,0 +1,24 @@
+package com.hotelbeds.supplierintegrations.hackertest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.hotelbeds.supplierintegrations.hackertest.PruebaTecnicaApplication;
+
+@SpringBootTest
+class PruebaTecnicaApplicationTest {
+
+	@Test
+	void testMain() {
+		Exception ex = null;
+		try {
+		PruebaTecnicaApplication.main(new String[] {});
+		}catch (Exception e) {
+			ex = e;
+		}
+		
+		assertThat(ex).as("testMain").isNull();
+	}
+}
