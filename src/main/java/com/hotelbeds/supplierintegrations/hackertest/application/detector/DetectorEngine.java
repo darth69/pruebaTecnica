74cbef746887c11786a0316faa3629b0fd3b4f1a
darth69 <torregrosa.f@gmail.com>
2022-07-24 02:12:18 +0200

    Feat: Limpiar CodeSmell

--- src/main/java/com/hotelbeds/supplierintegrations/hackertest/application/detector/DetectorEngine.java
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/application/detector/DetectorEngine.java
@@ -1,9 +1,10 @@
 package com.hotelbeds.supplierintegrations.hackertest.application.detector;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface DetectorEngine {
+public interface DetectorEngine extends Serializable{
 
 	boolean detectIp(List<LocalDateTime> events, LocalDateTime eventDateTime);
 
